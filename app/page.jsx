71e84372034f0610be8b9b96c3af2d8c19723c37
opd263 <omprakashdukiya543@gmail.com>
2024-05-16 22:35:20 +0530

    Initial commit

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,16 @@
-const Homepage = () => {
-  return (
-    <div>
-      Homepage
-    </div>
-  )
-}
+import Hero from '@/components/Hero';
+import InfoBoxes from '@/components/InfoBoxes';
+import HomeProperties from '@/components/HomeProperties';
+import FeaturedProperties from '@/components/FeaturedProperties';
 
-export default Homepage;
+const HomePage = () => {
+  return (
+    <>
+      <Hero />
+      <InfoBoxes />
+      <FeaturedProperties />
+      <HomeProperties />
+    </>
+  );
+};
+export default HomePage;
